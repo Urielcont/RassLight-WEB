1813568a0cca991bae90530823031fc8fc7f20e6
Adrian Rocha <adrianrochacon@gmail.com>
2024-03-01 09:39:54 -0600

    Se creo el esquema para el administrador de la pagina web

--- src/model/admin.model.js
+++ src/model/admin.model.js
@@ -1,1 +1,37 @@
-// Modelo de usuario administrador
+// Modelo de usuario 
+
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema= new mongoose.Schema({
+    nombres:{
+        type:String,
+        required: true
+    },
+    apPaterno:{
+        type:String,
+    },
+    apMaterno:{
+        type:String,
+    },
+    correo:{
+        type:String,
+        unique:true,
+        lowercase: true,
+        validate: (value) => {
+            return validator.isEmail(value)
+        }
+    },
+    password:{
+        type:String,
+        required:true,
+
+    },
+},
+     
+
+);
+
+const User = mongoose.model('User',userSchema);
+
+module.exports={User};
