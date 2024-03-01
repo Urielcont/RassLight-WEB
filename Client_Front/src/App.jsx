33e95eae2955225ea5fe7977da7ecc5a1d19ce2d
Urielcont <Urielalvaro3@gmail.com>
2024-02-29 20:18:33 -0600

    Ajustes en las rutas

--- Client_Front/src/App.jsx
+++ Client_Front/src/App.jsx
@@ -1,21 +1,20 @@
 // import { useState } from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css'
-import Loginpage from './Pages/login'
-
+import Loginpage from './pages/login'
+import Principal from './pages/principal'
 function App() {
 
   return (
-    <>
-       <BrowserRouter>
+      <BrowserRouter>
       <Routes>
         <Route path='/' element={<h1>Inicio</h1>}></Route>
         <Route path='/login' element={<Loginpage/>}></Route>
+        <Route path='/Principal' element={<Principal/>}></Route>
 
       </Routes>
     </BrowserRouter>
-    </>
-  )
+  );
 }
 
 export default App
---
+++ Client_Front/src/pages/principal.jsx
@@ -0,0 +1,14 @@
+// Pagina principal para mostrar datos del usuario
+// import {useForm} from "react-hook-form";
+// import { Link } from "react-router-dom";
+
+function Principal(){
+
+    return(
+        <div>
+            <h2>Hola Soy la Principal</h2>
+        </div>
+    );
+}
+
+export default Principal;
