54e341914a640f41e14d079df79adbdeacf6486b
MiguelSotoD <23soto330@gmail.com>
2024-03-03 20:53:59 -0600

    Login Terminado

--- Client_Front/src/pages/login.jsx
+++ Client_Front/src/pages/login.jsx
@@ -1,39 +1,44 @@
 import { useForm } from 'react-hook-form';
 
+
+// Importar las imagenes a autilizar
+import Logo from '../assets/images/logo.png';
+
+import '../css/login.css';
+
+
 function Loginpage() {
     const { register,formState: { errors } } = useForm();
    
     return (
-        <div className='flex justify-center items-center '>
-            <form  className='bg-slate-400 shadow-md rounded px-8 pt-6 pm-3 w-200'>
-                <h2 className='text-4xl text-center font-bold mb-4 text-black'>Login</h2>
-
-                <div className='mb-4'>
-                    <label className='bloc font-bold mb-2' htmlFor='username'>Username</label>
-                    <input type='text' {...register('username', { required: true })} id='username' className={` border  rounded w-full py-2 bg-sky-200 text-gray-700 `} placeholder='Username'/>
-                    {
-                    errors.username && (
-                        <p className='text-red-500'>Username es Requerido</p>
-                    )
-                }
-                </div>
-
-                
-                <div className='mb-6'>
-                    <label className='block' htmlFor='password'>Contraseña</label>
-                    <input type='password' {...register('password', { required: true })} id='password' className={` border  rounded w-full py-2 bg-sky-200 text-gray-700 `} placeholder='Contraseña'/>
-                    {
-                    errors.password && (
-                        <p className='text-red-500'>Contraseña es Requerido</p>
-                    )
-                }
-                </div>
-                
-                <div className='flex items-center justify-center'>
-                    <button type='submit' className= ''>Registrarse</button>
-                </div>
-            </form>
+<div className="bg-gray-200 h-screen flex items-center justify-center">
+    <div className="bg-white py-10 px-12 rounded-lg shadow-md">
+        <div className="flex items-center mb-4">
+        <img className="mr-4 h-20 w-auto" src={Logo} />
+        <h2 className="text-3xl font-semibold text-black">Login</h2>
         </div>
+        <div className="mb-4">
+            <label className=" block text-m font-semibold text-black" htmlFor="username">Username</label>
+            <input type="Usuarioinp" {...register('username', { required: true })} id="username" className="mt-1 w-full border rounded-md"  />
+            {errors.username && (
+                <p className="text-red-500">Username es Requerido</p>
+            )}
+        </div>
+
+        <div className="mb-6">
+            <label className="block text-m font-semibold text-black" htmlFor="password">Contraseña</label>
+            <input type="passwordinp" {...register('password', { required: true })} id="password" className="mt-1 w-full border rounded-md"  />
+            {errors.password && (
+                <p className="text-red-500">Contraseña es Requerido</p>
+            )}
+        </div>
+
+        <div className="flex items-center justify-center">
+            <button type="submitt" className="rounded-full text-white p-2 w-36 ">Ingresar</button>
+        </div>
+    </div>
+</div>
+
     );
 }
 
