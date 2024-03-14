8fecc0d41eb781f60dfa07eaa53b6c9b16ad57e2
Adrian Rocha <adrianrochacon@gmail.com>
2024-03-13 22:00:26 -0600

    Se protegio la ruta de mensajes con el token, y se actualizo la page principal

--- src/router/auth.route.js
+++ src/router/auth.route.js
@@ -6,7 +6,7 @@ const router = Router();
 router.post("/login", login);
 router.post("/logout", logout);
 router.post("/sendMessage", sendMessage);
-router.get("/getMessages", getMessages)
+router.get("/getMessages", authRequired, getMessages)
 router.get("/verify", verifyToken)
 router.get("/profile", authRequired, profile );
 export default router;
